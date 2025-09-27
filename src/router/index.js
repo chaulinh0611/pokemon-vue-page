2b5effb9ceadbe0edba2a1726f05ff7a93293a34
Nguyễn Châu Linh <chaulinh0611z@gmail.com>
2025-09-27 12:08:16 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -33,9 +33,11 @@ const routes = [
   { path: '/:name/:id', component: pokemonDetail},
 ];
 
+
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
+
 
 export default router;
